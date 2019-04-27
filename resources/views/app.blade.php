4d29f287802865ed5b748aa61add47c39b81cc66
Francine <francinemoraes08@hotmail.com>
2019-04-26 23:09:35 -0300

    layout padrão

---
+++ resources/views/app.blade.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    </head>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand text-white">Client Manager</a>
+        
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link {{(\Request::route()->getName() == 'employees.index' ? 'active' : '')}}" href="{{route('employees.index')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{(\Request::route()->getName() == 'employees.create' ? 'active' : '')}}" href="{{route('employees.create')}}">Novo</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+            </div>
+        </nav>
+    </header>
+    <body>
+        <div class="container" style="margin-top: 50px;">
+            @yield('content')
+        </div>
+    </body>
+</html>
