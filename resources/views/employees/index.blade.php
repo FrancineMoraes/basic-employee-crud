bf282fa53488b609d7f51b2ae87a72d7a7011719
Francine <francinemoraes08@hotmail.com>
2019-04-26 23:09:59 -0300

    criação da estrutra de create e view de index

---
+++ resources/views/employees/index.blade.php
@@ -0,0 +1,26 @@
+@extends('app')
+
+@section('content')
+<div class="col-md-12">
+    <h3>Clientes</h3>
+</div>
+<div class="col-md-12">
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">E-mail</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Rua</th>
+            <th scope="col">Nº</th>
+            <th scope="col">Bairro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+        </table>
+</div>
+@endsection
