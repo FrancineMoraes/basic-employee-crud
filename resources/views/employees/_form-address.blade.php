674057906ac0084df0f68d8bd18a40dae20e52b3
Francine <francinemoraes08@hotmail.com>
2019-04-28 19:51:20 -0300

    add or old

--- resources/views/employees/_form-address.blade.php
+++ resources/views/employees/_form-address.blade.php
@@ -1,25 +1,25 @@
 <h4>Endereço    <i class="fas fa-map-marked-alt"></i></h4>
 <div class="form-group col-md-4">
     <label for="cep">Cep</label>
-    <input type="text" name="cep" value="{{$employee->address->cep}}" class="form-control">
+    <input type="text" name="cep" value="{{$employee->address->cep or old('cep')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="street">Rua</label>
-    <input type="text" name="street" value="{{$employee->address->street}}" class="form-control">
+    <input type="text" name="street" value="{{$employee->address->street or old('street')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="number">Número</label>
-    <input type="text" name="number" value="{{$employee->address->number}}" class="form-control">
+    <input type="text" name="number" value="{{$employee->address->number or old('number')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="district">Bairro</label>
-    <input type="text" name="district" value="{{$employee->address->district}}" class="form-control">
+    <input type="text" name="district" value="{{$employee->address->district or old('district')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="city">Cidade</label>
-    <input type="text" name="city" value="{{$employee->address->city}}" class="form-control">
+    <input type="text" name="city" value="{{$employee->address->city or old('city')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="state">Estado</label>
-    <input type="text" name="state" value="{{$employee->address->state}}" class="form-control">
+    <input type="text" name="state" value="{{$employee->address->state or old('state')}}" class="form-control">
 </div>
--- resources/views/employees/_form.blade.php
+++ resources/views/employees/_form.blade.php
@@ -1,14 +1,14 @@
 <div class="form-group col-md-4">
     <label for="name">Nome</label>
-    <input type="text" name="name" value="{{$employee->name}}" class="form-control">
+    <input type="text" name="name" value="{{$employee->name or old ('name')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="email">Email</label>
-    <input type="email" name="email" value="{{$employee->email}}" class="form-control">
+    <input type="email" name="email" value="{{$employee->email or old('email')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     <label for="phone">Telefone</label>
-    <input type="text" name="phone" value="{{$employee->phone}}" class="form-control">
+    <input type="text" name="phone" value="{{$employee->phone or old('phone')}}" class="form-control">
 </div>
 <div class="form-group col-md-4">
     @isset($employee->image->path)
