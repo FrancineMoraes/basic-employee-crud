9a3bc6ce29d5d1d9c0ff8d6440562ccf8f9aafc8
Francine <francinemoraes08@hotmail.com>
2019-04-29 20:57:08 -0300

    add saida de erro no front

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,6 +37,15 @@
     </header>
     <body>
         <div class="container" style="margin-top: 50px;">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             @if (session('status'))
                 <div class="alert alert-{{session('status')}}">
                     {{ session('message') }}
